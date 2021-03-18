bb217067b9ea590879254e0ff9d10a992c5f182e
yourh3ro <nornikbiz@gmail.com>
2021-03-19 00:35:03 +0700

    Небольшие фиксы, убрал dlgs, убрал функцию Concatinate, создал dlgWindow и runPowershellScript

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.16
 require (
 	fyne.io/fyne v1.4.3 // indirect
 	fyne.io/fyne/v2 v2.0.1 // indirect
-	github.com/gen2brain/dlgs v0.0.0-20210222160047-2f436553172f // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20210202160940-bed99a852dfe // indirect
 	github.com/iamacarpet/go-win64api v0.0.0-20210311141720-fe38760bed28 // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
--- myIp.go
+++ myIp.go
@@ -3,35 +3,39 @@ package main
 import (
 	"bytes"
 	"errors"
-	"log"
 	"net"
 	"os"
+	"os/exec"
 	"regexp"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
-	"github.com/gen2brain/dlgs"
+
 	winapi "github.com/iamacarpet/go-win64api"
 	ps "github.com/mitchellh/go-ps"
 )
 
-// TODO: Отказаться от Concatinate
-// TODO: отказаться от dlgs
-// TODO: Кнопка убийства файрвола
+// TODO: пинговалка сервера, шлюза, Л44, для более быстрого определения неисправностей
+// TODO: Возможно - логгирование запусков Утилиты
+// TODO: точка входа для какого нибудь powershell скрипта
+// TODO: прокоментировать код
+
+// создание глобального экзепляра Fyne app
+var a = app.New()
 
 // Точка входа
 func main() {
 
 	ip, err := externalIP()
 	if err != nil {
-		err = nil
+		dlgWindow("Error!", err.Error(), "Ok")
 	}
 	ip = "Адрес: " + ip
 
 	hostname, err := os.Hostname()
 	if err != nil {
-		err = nil
+		dlgWindow("Error!", err.Error(), "Ok")
 	}
 	hostname = "Имя комп: " + hostname
 
@@ -76,17 +80,6 @@ func externalIP() (string, error) {
 	return "", errors.New("Нет сети!")
 }
 
-// Вспомогательная функция конкатинации строк
-func Concatinate(oneword string, twoword string) string {
-	strings := []string{oneword, twoword}
-	buffer := bytes.Buffer{}
-	for _, val := range strings {
-		buffer.WriteString(val)
-	}
-
-	return buffer.String()
-}
-
 // очистка кеша 1с
 func clearCash() {
 
@@ -94,15 +87,14 @@ func clearCash() {
 
 	processList, err := ps.Processes()
 	if err != nil {
-		log.Println("ps.Processes() Failed, are you using windows?")
-		return
+		dlgWindow("Error!", err.Error(), "Ok")
 	}
 
 	for x := range processList {
 		var process ps.Process
 		process = processList[x]
 		if process.Executable() == "1cv8c.exe" {
-			dlgs.Warning("Ошибка!", "Обнаружен процесс 1С:Предприятия! Закройте 1С и повторите попытку.")
+			dlgWindow("Ошибка!", "Обнаружен запущенный процесс 1С:Предприятия! Закройте 1С и повторите попытку.", "Ок")
 			ScriptMayWork = false
 		}
 	}
@@ -111,43 +103,43 @@ func clearCash() {
 
 		UserHomeDir, err := os.UserHomeDir()
 		if err != nil {
-			err = nil
+			dlgWindow("Error!", err.Error(), "Ok")
 		}
 
-		RoamingOneCDirPath := Concatinate(UserHomeDir, "/AppData/Roaming/1C/1cv8/")
-		LocalOneCDirPath := Concatinate(UserHomeDir, "/AppData/Local/1C/1cv8/")
+		RoamingOneCDirPath := UserHomeDir + "/AppData/Roaming/1C/1cv8/"
+		LocalOneCDirPath := UserHomeDir + "/AppData/Local/1C/1cv8/"
 
 		filesInRoaming, err := os.ReadDir(RoamingOneCDirPath)
 		if err != nil {
-			err = nil
+			dlgWindow("Error!", err.Error(), "Ok")
 		}
 
 		filesInLocal, err := os.ReadDir(LocalOneCDirPath)
 		if err != nil {
-			err = nil
+			dlgWindow("Error!", err.Error(), "Ok")
 		}
 
 		for _, fileInRoaming := range filesInRoaming {
 			matched, err := regexp.MatchString(`[\-]`, fileInRoaming.Name())
 			if err != nil {
-				err = nil
+				dlgWindow("Error!", err.Error(), "Ok")
 			}
 			if matched == true {
-				os.RemoveAll(Concatinate(RoamingOneCDirPath, fileInRoaming.Name()))
+				os.RemoveAll(RoamingOneCDirPath + fileInRoaming.Name())
 			}
 		}
 
 		for _, fileInLocal := range filesInLocal {
 			matched, err := regexp.MatchString(`[\-]`, fileInLocal.Name())
 			if err != nil {
-				err = nil
+				dlgWindow("Error!", err.Error(), "Ok")
 			}
 			if matched == true {
-				os.RemoveAll(Concatinate(LocalOneCDirPath, fileInLocal.Name()))
+				os.RemoveAll(LocalOneCDirPath + fileInLocal.Name())
 			}
 		}
 
-		dlgs.Info("Очистка кеша", "Кэш 1С очищен успешно!")
+		dlgWindow("Очистка кеша", "Кэш 1С очищен успешно!", "Ок")
 	}
 }
 
@@ -159,12 +151,12 @@ func RemoteConnection() {
 	winapi.FirewallDisable(3)
 	winapi.FirewallDisable(4)
 
-	dlgs.Info("Remote connection", "Настройка прошла успешно!")
+	dlgWindow("Remote connection", "Настройка прошла успешно!", "Ок")
 }
 
 // Отрисовка интерфейса
 func UI(ip string, hostname string) {
-	a := app.New()
+
 	w := a.NewWindow("Admin Tool")
 	w.Resize(fyne.NewSize(300, 100))
 	w.CenterOnScreen()
@@ -179,4 +171,35 @@ func UI(ip string, hostname string) {
 	))
 
 	w.ShowAndRun()
+}
+
+// Вывод информационного диалогового окна
+func dlgWindow(title string, content string, btnText string) {
+
+	windw := a.NewWindow(title)
+
+	label := widget.NewLabel(content)
+	windw.SetContent(widget.NewVBox(
+		label,
+		widget.NewButton(btnText, func() {
+			windw.Close()
+		}),
+	))
+
+	windw.Show()
+}
+
+// Запуск скрипта powershell по кнопке, с указанием пути к скрипту. Проверить, работает или нет вообще.
+func runPowershellFileScript(path string) string {
+	ps, _ := exec.LookPath("powershell.exe")
+	psArg := "-File" + path
+
+	command := exec.Command(ps, psArg)
+
+	var stdout bytes.Buffer
+	command.Stdout = &stdout
+
+	command.Run()
+
+	return stdout.String()
 }
